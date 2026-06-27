249698d56b01ef56aad17989772926058c4acd06
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-06-27 18:37:46 +0330

    adding new Feature it can save the watch list in localstorge.

--- src/app-v2.js
+++ src/app-v2.js
@@ -2,10 +2,55 @@
 ////////S10 E17////////
 ///////////////////////
 
-import {use, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import StarRating from "./starRating";
 
+const tempMovieData = [
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster:
+            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt0133093",
+        Title: "The Matrix",
+        Year: "1999",
+        Poster:
+            "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt6751668",
+        Title: "Parasite",
+        Year: "2019",
+        Poster:
+            "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+];
 
+const tempWatchedData = [
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster:
+            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        runtime: 148,
+        imdbRating: 8.8,
+        userRating: 10,
+    },
+    {
+        imdbID: "tt0088763",
+        Title: "Back to the Future",
+        Year: "1985",
+        Poster:
+            "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+        runtime: 116,
+        imdbRating: 8.5,
+        userRating: 9,
+    },
+];
 
 
 const average = (arr) =>
@@ -16,20 +61,10 @@ const KEY = "23a9a4e5"
 export default function App() {
     const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
-
+    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
     const [selectedId, setSelectedId] = useState(null);
-
-    // const [watched, setWatched] = useState([]);
-    const [watched, setWatched] = useState(function () {
-        const storedValue = localStorage.getItem("watched");
-        return JSON.parse(storedValue);
-    });
-
-    useEffect(() => {
-        localStorage.setItem("watched",JSON.stringify(watched));
-    }, [watched]);
 
 
     useEffect(function () {
@@ -89,10 +124,7 @@ export default function App() {
     }
 
     function handelAddWatch(movie) {
-        setWatched((watched) => [...watched, movie]);
-
-        // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
-
+        setWatched((watched) => [...watched, movie])
     }
 
     function handelDeleteWatch(id) {
@@ -200,6 +232,30 @@ function Box({children}) {
 }
 
 
+// function WatchedBox() {
+//
+//
+//     const [watched, setWatched] = useState(tempWatchedData);
+//     const [isOpen2, setIsOpen2] = useState(true);
+//
+//
+//     return <div className="box">
+//         <button
+//             className="btn-toggle"
+//             onClick={() => setIsOpen2((open) => !open)}
+//         >
+//             {isOpen2 ? "–" : "+"}
+//         </button>
+//         {isOpen2 && (
+//             <>
+//                 <WatchedSummary watched={watched}/>
+//                 <WatchedMovieList watched={watched}/>
+//
+//
+//             </>
+//         )}
+//     </div>
+// }
 
 function MovieList({movies, onSelectMovie}) {
 
